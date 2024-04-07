94a751bd39bda3a70e14f9805a3e225eeaf44bed
Cameron Maiden <cmaiden42@g.ucla.edu>
2024-04-07 12:44:22 -0700

    Added City class declaration to City.h, Added manifest constants to globals.h

--- s24project1/City.h
+++ s24project1/City.h
@@ -8,6 +8,47 @@
 #ifndef City_h
 #define City_h
 
-#include <stdio.h>
+#include "globals.h"
+
+///////////////////////////////////////////////////////////////////////////
+// Type definitions
+///////////////////////////////////////////////////////////////////////////
+
+class Player;
+class Tooter;
+
+class City
+{
+  public:
+        // Constructor/destructor
+    City(int nRows, int nCols);
+    ~City();
+
+        // Accessors
+    int     rows() const;
+    int     cols() const;
+    Player* player() const;
+    bool    isPlayerAt(int r, int c) const;
+    int     tooterCount() const;
+    int     nTootersAt(int r, int c) const;
+    bool    determineNewPosition(int& r, int& c, int dir) const;
+    void    display() const;
+
+        // Mutators
+    bool addTooter(int r, int c);
+    bool addPlayer(int r, int c);
+    void preachToTootersAroundPlayer();
+    void moveTooters();
+
+  private:
+    int     m_rows;
+    int     m_cols;
+    Player* m_player;
+    Tooter* m_tooters[MAXTOOTERS];
+    int     m_nTooters;
+
+      // Helper functions
+    bool isInBounds(int r, int c) const;
+};
 
 #endif /* City_hpp */
--- s24project1/main.cpp
+++ s24project1/main.cpp
@@ -13,59 +13,11 @@
 using namespace std;
 
 ///////////////////////////////////////////////////////////////////////////
-// Manifest constants
-///////////////////////////////////////////////////////////////////////////
-
-const int MAXROWS = 20;               // max number of rows in a city
-const int MAXCOLS = 30;               // max number of columns in a city
-const int MAXTOOTERS = 125;           // max number of Tooters allowed
-const int INITIAL_PLAYER_HEALTH = 12;
-
-const int UP      = 0;
-const int DOWN    = 1;
-const int LEFT    = 2;
-const int RIGHT   = 3;
-const int NUMDIRS = 4;
-
-///////////////////////////////////////////////////////////////////////////
 // Type definitions
 ///////////////////////////////////////////////////////////////////////////
 #include "Tooter.h"
 #include "Player.h"
-
-class City
-{
-  public:
-        // Constructor/destructor
-    City(int nRows, int nCols);
-    ~City();
-
-        // Accessors
-    int     rows() const;
-    int     cols() const;
-    Player* player() const;
-    bool    isPlayerAt(int r, int c) const;
-    int     tooterCount() const;
-    int     nTootersAt(int r, int c) const;
-    bool    determineNewPosition(int& r, int& c, int dir) const;
-    void    display() const;
-
-        // Mutators
-    bool addTooter(int r, int c);
-    bool addPlayer(int r, int c);
-    void preachToTootersAroundPlayer();
-    void moveTooters();
-
-  private:
-    int     m_rows;
-    int     m_cols;
-    Player* m_player;
-    Tooter* m_tooters[MAXTOOTERS];
-    int     m_nTooters;
-
-      // Helper functions
-    bool isInBounds(int r, int c) const;
-};
+#include "City.h"
 
 class Game
 {
@@ -579,6 +531,14 @@ int main()
 //    if (false)  // so that this test builds, but does not write anything
 //        Tooter t(nullptr, 1, 1);
 //}
+
+//#include "Player.h"
+//    int main()
+//    {
+//        if (false)  // so that this test builds, but does not write anything
+//            Player p(nullptr, 1, 1);
+//    }
+
 
 ///////////////////////////////////////////////////////////////////////////
 //  clearScreen implementation
