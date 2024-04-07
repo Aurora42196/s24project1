7462ffc8e6dbd46541785ff56195b32bd79f5bda
Cameron Maiden <cmaiden42@g.ucla.edu>
2024-04-06 20:28:36 -0700

    Moved the Tooter class declaration into Tooter.h

--- s24project1/Tooter.h
+++ s24project1/Tooter.h
@@ -8,6 +8,26 @@
 #ifndef Tooter_h
 #define Tooter_h
 
-#include <stdio.h>
+class City;  // This is needed to let the compiler know that City is a
+             // type name, since it's mentioned in the Tooter declaration.
+
+class Tooter
+{
+  public:
+        // Constructor
+    Tooter(City* cp, int r, int c);
+
+        // Accessors
+    int  row() const;
+    int  col() const;
+
+        // Mutators
+    void move();
+
+  private:
+    City* m_city;
+    int   m_row;
+    int   m_col;
+};
 
 #endif /* Tooter_hpp */
--- s24project1/main.cpp
+++ s24project1/main.cpp
@@ -31,27 +31,9 @@ const int NUMDIRS = 4;
 // Type definitions
 ///////////////////////////////////////////////////////////////////////////
 
+#include "Tooter.h"
 class City;  // This is needed to let the compiler know that City is a
              // type name, since it's mentioned in the Tooter declaration.
-
-class Tooter
-{
-  public:
-        // Constructor
-    Tooter(City* cp, int r, int c);
-
-        // Accessors
-    int  row() const;
-    int  col() const;
-
-        // Mutators
-    void move();
-
-  private:
-    City* m_city;
-    int   m_row;
-    int   m_col;
-};
 
 class Player
 {
